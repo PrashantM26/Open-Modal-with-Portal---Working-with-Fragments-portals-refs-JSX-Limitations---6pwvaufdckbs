b95c5bbbbdddf36774aff2c2b6c67885be3de6d5
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-03 21:20:40 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,11 @@ const HomePage = () => {
 
   return (
     <div>
+      <button onClick={openModal}>Open Modal</button>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <h2>This is a Modal</h2>
         <p>Using React Portals with Next.js</p>
       </Modal>
-      <button onClick={openModal}>Open Modal</button>
     </div>
   );
 };
